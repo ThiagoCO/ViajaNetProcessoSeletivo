6fa11865484a65992fa1ad76e804ae1bb2808ed1
Thiago Cavalcante <thiago.cavalcante15@hotmail.com>
2019-02-04 19:29:10 -0200

    inclusao do gerenciador de projeto CocoaPods + Alamofire

---
+++ ViajaNetTest/Service/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  ViajaNetTest
+//
+//  Created by Thiago Cavalcante de Oliveira on 04/02/19.
+//  Copyright © 2019 Thiago Cavalcante de Oliveira. All rights reserved.
+//
+
+import Foundation
